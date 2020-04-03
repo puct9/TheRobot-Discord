385a292ee07c9d7bbf955e5af04a989d4eb26af3
Luke Yang <lukeybsn@gmail.com>
2020-04-03 20:32:15 +1100

    Improved

--- tf2_utils/botutils.py
+++ tf2_utils/botutils.py
@@ -6,12 +6,13 @@ import discord
 
 from routing.mapper import Endpoint
 from .utils import parse_game_info
+from .utils import get_player_info as raw_get_player_info
 
 
 TF2_ADDR = os.environ.get('tf2_addr')
 
-# allow arbitrary ports 5000-5049
-SUBPROC_PORTS = {port: True for port in range(5000, 5050)}
+# allow arbitrary ports
+SUBPROC_PORTS = {port: True for port in range(30000, 30050)}
 
 
 @Endpoint
@@ -34,17 +35,21 @@ async def get_player_info(client: discord.Client,
     # we don't have to shut it down manually
     subprocess.Popen(['python', './tf2_utils/tf2gamequery.py', str(port)])
 
-    reader, writer = await asyncio.open_connection(host='127.0.0.1', port=port)
-    print('opened conn')
-    writer.write(TF2_ADDR.encode())
-    await writer.drain()
-    print('specified info')
-    info = await reader.read()
-    SUBPROC_PORTS[port] = True  # released the port by now
-    print(info)
-    player_info = parse_game_info(info)
-    print(player_info)
+    try:
+        reader, writer = await asyncio.open_connection(host='127.0.0.1',
+                                                       port=port)
+        writer.write(TF2_ADDR.encode())
+        await writer.drain()
+        info = await reader.read()
+        SUBPROC_PORTS[port] = True  # released the port by now
+    except Exception:
+        SUBPROC_PORTS[port] = True
+        # fallback to non-async method
+        addr = TF2_ADDR.split(':')
+        addr[1] = int(addr[1])
+        info = raw_get_player_info(tuple(addr))
 
+    player_info = parse_game_info(info)
     msg = 'Players currently on the server\n```\n'
     if player_info:
         msg += '\n'.join(f'{player[0]} - {player[1]}' for player in player_info)

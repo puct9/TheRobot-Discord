d598bf79acfdcf9e7f07a7d05094fb009c39e493
Luke Yang <lukeybsn@gmail.com>
2020-11-08 03:53:26 +1100

    Add graph feature

--- ai_utils/msgs.py
+++ ai_utils/msgs.py
@@ -5,4 +5,7 @@ from . import botutils
 msgpatterns = [
     Pattern(r'^\$ai gender [a-zA-Z]{1,16}$', botutils.predict_gender,
             description='Guess the gender of a name'),
+
+    Pattern(r'^\$ai model gender$', botutils.get_summary,
+            description='Show the architecture of the model'),
 ]
